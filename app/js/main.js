c6b2763f341edb8069726999a4ff9c14a2068a7c
Diego Pablo Matias Baltar <dpmbaltar@gmail.com>
2023-01-02 00:39:27 -0300

    Agregar cache local

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,14 @@
 const app = new Navigo('http://localhost:9000/', true, '#!');
 
 app.on('/weather/current', () => {
+  const cache = getCache('current')
+
+  if (cache) {
+    togglePlaceholder('current-weather', 'current-placeholder', false)
+    activateSection('current-section')
+    return
+  }
+
   togglePlaceholder('current-weather', 'current-placeholder')
   activateSection('current-section')
   getGeolocation((position) => {
@@ -13,12 +21,30 @@ app.on('/weather/current', () => {
       .then(response => response.json())
       .then(response => {
         console.log(response)
-        const weather = response
+        setCache('current', response)
         const location = response.location
         const current = response.current
         const section = document.getElementById('current-section')
         const entries = {
-          last_updated: () => {
+          location:       () => [location.name, location.region, location.country].join(', '),
+          temp_c:         () => current.temp_c + ' &deg;C',
+          temp_f:         () => current.temp_f + ' &deg;F',
+          condition_text: () => current.condition.text,
+          wind_kph:       () => current.wind_kph + ' km/h',
+          wind_mph:       () => current.wind_mph + ' mph',
+          wind_dir:       () => current.wind_dir,
+          pressure_mb:    () => current.pressure_mb + ' hPa',
+          pressure_in:    () => current.pressure_in + ' inHg',
+          precip_mm:      () => current.precip_mm + ' mm',
+          precip_in:      () => current.precip_in + ' in',
+          humidity:       () => current.humidity + '%',
+          cloud:          () => current.cloud + '%',
+          feelslike_c:    () => current.feelslike_c + ' &deg;C',
+          feelslike_f:    () => current.feelslike_f + ' &deg;F',
+          vis_km:         () => current.vis_km + ' km',
+          vis_miles:      () => current.vis_miles + '  miles',
+          uv:             () => current.uv,
+          last_updated:   () => {
             const date = new Date(current.last_updated)
             const options = {
               weekday: 'long',
@@ -30,10 +56,6 @@ app.on('/weather/current', () => {
             }
             return date.toLocaleString('es-AR', options)
           },
-          location: () => `${location.name}, ${location.region}, ${location.country}`,
-          temp_c: () => current.temp_c + ' &deg;C',
-          temp_f: () => current.temp_f + ' &deg;F',
-          condition_text: () => current.condition.text,
           condition_icon: (e) => {
             let icon = 'images/64x64/'
             icon += current.is_day ? 'day/' : 'night/'
@@ -42,26 +64,12 @@ app.on('/weather/current', () => {
             e.alt = current.condition.text
             return null
           },
-          wind_kph: () => current.wind_kph + ' km/h',
-          wind_mph: () => current.wind_mph + ' mph',
-          wind_dir: () => current.wind_dir,
           wind_dir_icon: (e) => {
             const icon = document.createElement('i')
             icon.classList.add('bi')
             icon.classList.add('bi-arrow-down')
             return icon.outerHTML
           },
-          pressure_mb: () => current.pressure_mb + ' hPa',
-          pressure_in: () => current.pressure_in + ' inHg',
-          precip_mm: () => current.precip_mm + ' mm',
-          precip_in: () => current.precip_in + ' in',
-          humidity: () => current.humidity + '%',
-          cloud: () => current.cloud + '%',
-          feelslike_c: () => current.feelslike_c + ' &deg;C',
-          feelslike_f: () => current.feelslike_f + ' &deg;F',
-          vis_km: () => current.vis_km + ' km',
-          vis_miles: () => current.vis_miles + '  miles',
-          uv: () => current.uv,
         }
 
         Object.keys(entries).forEach((key) => {
@@ -84,7 +92,7 @@ app.on('/weather/forecast', () => {
 })
 
 app.on('*', () => {
-  router.navigate('/weather/current')
+  app.navigate('/weather/current')
 })
 
 app.resolve()
--- app/js/util.js
+++ app/js/util.js
@@ -1,13 +1,35 @@
 
-let _geolocation = null
+const _cache = {}
+const _cache_ttl = 60*60*1000
+const _geolocation = { position: null }
+
+function getCache(key) {
+  if (!_cache[key])
+    return null
+
+  const { timestamp, value } = _cache[key]
+  const elapsedTime = (new Date().getTime() - timestamp)
+
+  if (elapsedTime > _cache_ttl) {
+    delete _cache[key]
+    return null
+  }
+
+  return value
+}
+
+function setCache(key, value) {
+  const date = new Date()
+  _cache[key] = { timestamp: date.getTime(), value: value }
+}
 
 function getGeolocation(showPosition) {
-  if (_geolocation)
-    return showPosition(_geolocation)
+  if (_geolocation.position)
+    return showPosition(_geolocation.position)
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
-      _geolocation = position
+      _geolocation.position = position
       showPosition(position)
     })
   } else {
